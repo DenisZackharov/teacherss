d3cea74aeb33f4d25982d1b39814b015ec4d45f7
deniszackharov <denis.zaharov@flatstack.com>
2022-10-19 22:51:07 +0300

    remove comments

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,47 +1,12 @@
-# frozen_string_literal: true
-
-# Please do not make direct changes to this file!
-# This generator is maintained by the community around simple_form-bootstrap:
-# https://github.com/rafaelfranca/simple_form-bootstrap
-# All future development, tests, and organization should happen there.
-# Background history: https://github.com/heartcombo/simple_form/issues/1561
-
-# Uncomment this and change the path if necessary to include your own
-# components.
-# See https://github.com/heartcombo/simple_form#custom-components
-# to know more about custom components.
-# Dir[Rails.root.join("lib/components/**/*.rb")].each { |f| require f }
-
-# Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config| # rubocop:disable Metrics/BlockLength
-  # Default class for buttons
   config.button_class = "btn"
-
-  # Define the default class of the input wrapper of the boolean input.
   config.boolean_label_class = "form-check-label"
-
-  # How the label text should be generated altogether with the required text.
   config.label_text = ->(label, required) { "#{label} #{required}" }
-
-  # Define the way to render check boxes / radio buttons with labels.
   config.boolean_style = :inline
-
-  # You can wrap each item in a collection of radio/check boxes with a tag
   config.item_wrapper_tag = :div
-
-  # Defines if the default input wrapper class should be included in radio
-  # collection wrappers.
   config.include_default_input_wrapper_class = false
-
-  # CSS class to add for error notification helper.
   config.error_notification_class = "alert alert-danger"
-
-  # Method used to tidy up errors. Specify any Rails Array method.
-  # :first lists the first message for each field.
-  # :to_sentence to list all errors for each field.
   config.error_method = :to_sentence
-
-  # add validation classes to `input_field`
   config.input_field_error_class = "is-invalid"
   config.input_field_valid_class = "is-valid"
 
@@ -413,11 +378,8 @@ SimpleForm.setup do |config| # rubocop:disable Metrics/BlockLength
     b.use :hint, wrap_with: { tag: "small", class: "form-text text-muted" }
   end
 
-  # The default wrapper to be used by the FormBuilder.
   config.default_wrapper = :vertical_form
 
-  # Custom wrappers for input types. This should be a hash containing an input
-  # type as key and the wrapper that will be used for all inputs with specified type.
   config.wrapper_mappings = {
     boolean: :vertical_boolean,
     check_boxes: :vertical_collection,
@@ -428,16 +390,4 @@ SimpleForm.setup do |config| # rubocop:disable Metrics/BlockLength
     range: :vertical_range,
     time: :vertical_multi_select
   }
-
-  # enable custom form wrappers
-  # config.wrapper_mappings = {
-  #   boolean:       :custom_boolean,
-  #   check_boxes:   :custom_collection,
-  #   date:          :custom_multi_select,
-  #   datetime:      :custom_multi_select,
-  #   file:          :custom_file,
-  #   radio_buttons: :custom_collection,
-  #   range:         :custom_range,
-  #   time:          :custom_multi_select
-  # }
 end

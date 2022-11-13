713104bb382e94ba15a58416f97798292d104831
deniszackharov <denis.zaharov@flatstack.com>
2022-11-13 23:12:09 +0300

    review fix

--- app/controllers/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  def create
-    build_resource(sign_up_params)
-
-    resource.save
-    yield resource if block_given?
-    redirect_to root_path
-  end
-end

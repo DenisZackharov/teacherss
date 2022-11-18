bdd27cea5a6a5fe5eca7427eca251b7f33c70690
deniszackharov <denis.zaharov@flatstack.com>
2022-11-18 20:20:49 +0300

    fix from review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   extend Enumerize
 
+  MAX_HOURS_PER_WEEK = 35
   ROLES = %i[super_admin teacher head_teacher director].freeze
 
   # TODO: connect :confirmable when email sender will work
@@ -12,7 +13,11 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :email, presence: true
   validates :minimum_working_hours_per_week, :maximum_working_hours_per_week,
-            numericality: { greater_than_or_equal_to: 0 }, on: :update
+            numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: MAX_HOURS_PER_WEEK }, on: :update
+  validates :minimum_working_hours_per_week, comparison: { less_than: :maximum_working_hours_per_week },
+                                             if: :maximum_working_hours_per_week
+  validates :maximum_working_hours_per_week, comparison: { greater_than: :minimum_working_hours_per_week },
+                                             if: :minimum_working_hours_per_week
 
-  enumerize :role, in: ROLES, predicates: true, scope: :shallow, default: :super_admin
+  enumerize :role, in: ROLES, predicates: true, scope: :shallow, default: :director
 end

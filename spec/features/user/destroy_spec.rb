098e3f62f2b59020f83af3f6944bbfd7036c9b51
deniszackharov <denis.zaharov@flatstack.com>
2022-12-12 00:20:27 +0300

    add tests

---
+++ spec/features/user/destroy_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+feature "Destroy" do
+  before do
+    create(
+      :user,
+      email: "example@gmail.com",
+      password: "123456",
+      role: "director",
+      first_name: "Bilbo",
+      last_name: "Baggins"
+    )
+  end
+
+  scenario "edit with current password" do
+    sign_in("example@gmail.com", "123456")
+    click_on "Bilbo Baggins"
+
+    click_on "Delete my account"
+
+    expect(page).to have_content("Bye! Your account has been successfully cancelled. We hope to see you again soon.")
+    expect(page).to have_content("Sign In")
+  end
+end
---
+++ spec/features/user/edit_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+feature "Edit" do
+  before do
+    create(
+      :user,
+      email: "example@gmail.com",
+      password: "123456",
+      role: "director",
+      first_name: "Bilbo",
+      last_name: "Baggins"
+    )
+  end
+
+  scenario "edit with current password" do
+    sign_in("example@gmail.com", "123456")
+    click_on "Bilbo Baggins"
+
+    fill_in "user_first_name", with: "John"
+    fill_in "user_last_name", with: "Doe"
+    fill_in "user_current_password",	with: "123456"
+
+    click_on "Update"
+
+    expect(page).to have_content("Your account has been updated successfully.")
+    expect(page).to have_content("John Doe")
+  end
+
+  scenario "edit without current password" do
+    sign_in("example@gmail.com", "123456")
+    click_on "Bilbo Baggins"
+
+    fill_in "user_first_name", with: "John"
+    fill_in "user_last_name", with: "Doe"
+
+    click_on "Update"
+
+    expect(page).to have_content("Current password can't be blank")
+  end
+end

282548b81a6c820168a674f15542b8d651131bb3
deniszackharov <denis.zaharov@flatstack.com>
2023-03-08 18:49:36 +0300

    add company name when user signed in

--- spec/features/authenticate/sign_in_spec.rb
+++ spec/features/authenticate/sign_in_spec.rb
@@ -1,13 +1,23 @@
 require "rails_helper"
 
 feature "Sign In" do
+  let!(:company) { create(:company, name: "School 13") }
+
   before do
-    create(:user, email: "example@gmail.com", password: "123456", first_name: "Bilbo", last_name: "Baggins")
+    create(
+      :user,
+      company:,
+      email: "example@gmail.com",
+      password: "123456",
+      first_name: "Bilbo",
+      last_name: "Baggins"
+    )
   end
 
   scenario "with valid credentials" do
     sign_in("example@gmail.com", "123456")
     expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content("School 13")
   end
 
   scenario "with invalid credentials" do

beeba1419cf70d5885f389858c36d7488e8321a0
deniszackharov <denis.zaharov@flatstack.com>
2023-03-18 20:21:41 +0300

    only director see company

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,5 @@
 class UserDecorator < ApplicationDecorator
-  delegate :id, :first_name, :last_name, :middle_name, :role, :organization
+  delegate :id, :first_name, :last_name, :middle_name, :role, :organization, :director?
 
   def first_name_last_name
     "#{object.first_name} #{object.last_name}"
